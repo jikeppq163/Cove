b68b6fdc4cf724a32670d7ca6e05b944f2a4632b
欧元 <32092244+jikeppq163@users.noreply.github.com>
2022-04-16 15:24:41 +0800

    20220416

--- pages/stars/index.vue
+++ pages/stars/index.vue
@@ -1,17 +1,42 @@
 <template>
-	<view>stars</view>
+	<view>
+		<view class="u-p-10 bg-blue u-font-white view_animation">灰色</view>
+	</view>
 </template>
 
 <script>
-	// 生成表单的 工具 https://demo.ucharts.cn/#/
+// 生成表单的 工具 https://demo.ucharts.cn/#/
+
+
 export default {
 	data(){
 		return{
+			
 		}
+	},
+	mounted() {
+		
 	}
 }
 </script>
 
-<style>
+<style scope lang="scss">
+	.view_animation{
+		width:100px;
+		height:50px;
+		background:red;
+		color:white;
+		font-weight:bold;
+		position:relative;
+		animation:mymove 5s infinite;
+		animation-timing-function:ease-in;
+	}
+	
+	@keyframes mymove
+	{
+		from {left:-100px;top: -100px;}
+		to {left:400px;top: 300px;}
+	}
+	
 </style>
 
