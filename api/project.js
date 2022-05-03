4e60f3fd5142017e173d80a3f5472311dc016870
欧元 <32092244+jikeppq163@users.noreply.github.com>
2022-05-03 18:40:19 +0800

    20220503

---
+++ api/project.js
@@ -0,0 +1,33 @@
+import request from './request.js'
+
+let project = {
+	create: function (){
+		
+	},
+	
+	star:function (){
+		
+	},
+	
+	delete:function (){
+		
+	},
+	
+	view:function(){
+		
+	},
+	
+	update:function(){
+		
+	},
+	
+	list:function(){
+		
+	},
+	
+	share:function(){
+		
+	}
+}
+
+export default project
---
+++ api/story.js
@@ -0,0 +1,21 @@
+import request from './request.js'
+
+let story = {
+	create:function(){
+		
+	},
+	
+	like:function(){
+		
+	},
+	
+	byProject:function(){
+		
+	},
+	
+	delete:function(){
+		
+	}
+}
+
+export default story
