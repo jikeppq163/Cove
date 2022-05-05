91ebfe91e31ec468d4a29f02b21d3a93f86ca259
欧元 <32092244+jikeppq163@users.noreply.github.com>
2022-05-05 16:10:25 +0800

    20220505 修正社区样式

--- pages/stars/index.vue
+++ pages/stars/index.vue
@@ -6,52 +6,54 @@
 		<view class="flex space-between u-font-gray2 u-p-l-20 u-p-r-20">
 			 <liuyuno-tabs :tabData="titleTag" :defaultIndex="defaultIndex" @tabClick='tabClick' />
 		</view>
-		<view class="u-p-t-20">
-			<swiper indicator-dots :current="swiperCurrent" previous-margin='40px' next-margin='40px' style="height: 1000rpx;" >
+		<view class="">
+			<swiper indicator-dots :current="swiperCurrent" previous-margin='40px' next-margin='40px' :style="style" >
 				<swiper-item v-for="(item,index) of swiperData" :key="item.id" @click="handleClickSwiper(index)">
-					<view class="flex center u-p-t-40 u-m-b-20">
-						<view class="u-bg-malandy-g1 u-radius-20 shadow-blur" style="width: 500rpx;height: 800rpx;">
-							<view class="flex u-p-10">
-								<view class="u-p-10">
-									<view class="u-radius-20" style="width: 120rpx;height: 120rpx;overflow: hidden;">
-										<img :src="item.userInfo.avatarUrl" style="width: 100%;height: 100%;" alt="">
+					<view class="flex center" style="height: 100%;">
+						<view class="flex-column center" style="height: 100%;">
+							<view class="u-bg-malandy-g1 u-radius-20 shadow-blur" style="width: 500rpx;height: 900rpx;">
+								<view class="flex u-p-10">
+									<view class="u-p-10">
+										<view class="u-radius-20" style="width: 120rpx;height: 120rpx;overflow: hidden;">
+											<img :src="item.userInfo.avatarUrl" style="width: 100%;height: 100%;" alt="">
+										</view>
+									</view>
+									<view class="text-center u-font-gray4 u-p-10 flex-column center nowrap" >
+										<text class="u-p-3 u-font-size-14" >
+											{{item.rdata.title}}
+										</text>
+										<text class="u-p-3 u-font-size-12 nowrap" >
+											{{item.userInfo.nickName}}
+										</text>
+										<view class="">
+											<uni-icons type="location" color='#fff'></uni-icons>
+											<text class="u-p-3 nowrap">{{item.rdata.location}}</text>
+										</view>
 									</view>
 								</view>
-								<view class="text-center u-font-gray4 u-p-10 flex-column center nowrap" >
-									<text class="u-p-3 u-font-size-14" >
-										{{item.rdata.title}}
-									</text>
-									<text class="u-p-3 u-font-size-12 nowrap" >
-										{{item.userInfo.nickName}}
-									</text>
-									<view class="">
-										<uni-icons type="location" color='#fff'></uni-icons>
-										<text class="u-p-3 nowrap">{{item.rdata.location}}</text>
+								<view class="text-center u-p-t-20 u-font-gray2">
+									<text class="u-font-size-30">“</text>
+									<text class="u-font-gray4">{{item.rdata.thoughts}} </text>
+									<text class="u-font-size-30">”</text>
+								</view>
+								<view class="u-m-10 u-m-t-20 u-p-40 u-bg-maka-g text-center u-font-gray2 shadow-lg">
+									配图
+								</view>
+								<view class="flex absolute bottom-40">
+									<view class="u-p-5 u-m-l-10 u-font-white u-bg-maka-g2 u-radius-5" v-for="item_mood of item.rdata.mood" :key='item_mood.id'>
+										{{item_mood}}
 									</view>
 								</view>
-							</view>
-							<view class="text-center u-p-t-20 u-font-gray2">
-								<text class="u-font-size-30">“</text>
-								<text class="u-font-gray4">{{item.rdata.thoughts}} </text>
-								<text class="u-font-size-30">”</text>
-							</view>
-							<view class="u-m-10 u-m-t-20 u-p-40 u-bg-maka-g text-center u-font-gray2 shadow-lg">
-								配图
-							</view>
-							<view class="flex absolute bottom-40">
-								<view class="u-p-5 u-m-l-10 u-font-white u-bg-maka-g2 u-radius-5" v-for="item_mood of item.rdata.mood" :key='item_mood.id'>
-									{{item_mood}}
+								<view class="absolute left-20 bottom-10" style="">
+									<view class="flex u-font-gray4 u-font-size-16">
+										<uni-icons type="chat" color='#fff' size="20"></uni-icons>
+										{{item.comment}}
+									</view>
 								</view>
-							</view>
-							<view class="absolute left-20 bottom-10" style="">
-								<view class="flex u-font-gray4 u-font-size-16">
-									<uni-icons type="chat" color='#fff' size="20"></uni-icons>
-									{{item.comment}}
-								</view>
-							</view>
-							<view class="absolute right-20 bottom-10" style="">
-								<view class="flex u-font-gray4 u-font-size-16" @click="goto('/pages/share/index')">
-									详情
+								<view class="absolute right-20 bottom-10" style="">
+									<view class="u-p-l-10 u-p-r-10 u-radius-3 uni-shadow-lg u-bg-maka2 u-bg-white u-font-gray4 text-center" @click="goto('/pages/share/index')">
+										详情
+									</view>
 								</view>
 							</view>
 						</view>
@@ -72,13 +74,18 @@
 				titleTag:['生活','情感','校园','职场','其他'],
 				defaultIndex:0,
 				swiperCurrent:1,
-				swiperData: []
+				swiperData: [],
+				style:{
+					"height":'1200rpx'
+				}
 			}
 		},
 		components: {
 		        liuyunoTabs
 		},
 		onShow(){
+			this.style.height = (this.$store.getters.getWindowsHeight*2 *0.8 ) +'rpx';
+			console.log('defaultHeight',this.style);
 			var that = this;
 			reqProject.star({
 				success:(res)=>{
