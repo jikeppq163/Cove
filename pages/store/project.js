2cfcae3ba6d530e6a5da4554195b71160723da9f
SighOfFrostmourne <50022214+SighOfFrostmourne@users.noreply.github.com>
2022-05-31 20:07:34 +0800

    Reverse synth permutation

--- pages/store/project.js
+++ pages/store/project.js
@@ -6,7 +6,7 @@ import getInstrument from './instrument';
 import getNoteAtHeight from './getNode';
 
 const pctArray =['D2', 'E2', 'F#2', 'G2', 'A2', 'B2', 'C#3', 'D3', 'E3', 'F#3', 'G3', 'A3', 'B3', 'C#4', 'D4', 'E4', 'F#4', 'G4', 'A4', 'B4', 'C#5', 'D5', 'E5', 'F#5', 'G5', 'A5', 'B5', 'C#6', 'D6', 'E6', 'F#6', 'G6', 'A6', 'B6', 'C#7'];
-const notesArray = ['A0', 'A#0', 'B0', 
+const notesArray_lowToHigh = ['A0', 'A#0', 'B0', 
 'C1', 'C#1', 'D1', 'D#1', 'E1', 'F1', 'F#1', 'G1', 'G#1', 'A1', 'A#1', 'B1',
 'C2', 'C#2', 'D2', 'D#2', 'E2', 'F2', 'F#2', 'G2', 'G#2', 'A2', 'A#2', 'B2',
 'C3', 'C#3', 'D3', 'D#3', 'E3', 'F3', 'F#3', 'G3', 'G#3', 'A3', 'A#3', 'B3', 
@@ -14,6 +14,7 @@ const notesArray = ['A0', 'A#0', 'B0',
 'C5', 'C#5', 'D5', 'D#5', 'E5', 'F5', 'F#5', 'G5', 'G#5', 'A5', 'A#5', 'B5', 
 'C6', 'C#6', 'D6', 'D#6', 'E6', 'F6', 'F#6', 'G6', 'G#6', 'A6', 'A#6', 'B6', 
 'C7', 'C#7'];//, 'D7', 'D#7', 'E7', 'F7', 'F#7', 'G7', 'G#7', 'A7', 'A#7', 'B7', 'C8']
+const notesArray = notesArray_lowToHigh.reverse();
 
 let state={
 	version:'0.2.4',
